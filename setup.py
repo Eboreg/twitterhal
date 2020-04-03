8e009f2adf80c511e64d8c9b945112a3fc10beea
Robert Huselius <robert@huseli.us>
2020-04-03 23:46:21 +0200

    Initial stuff

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import setuptools
+
+
+with open(os.path.join(os.path.dirname(__file__)), "README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="twitterhal",
+    version="0.0.1",
+    author="Robert Huselius",
+    author_email="robert@huseli.us",
+    description="A MegaHAL bot for Twitter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Eboreg/twitterhal",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "megahal>=0.3",
+        "python-Levenshtein",
+        "python-twitter",
+        "emoji",
+        "detectlanguage",
+    ],
+    dependency_links=[
+        "https://github.com/Eboreg/megahal/tarball/master#egg=megahal-0.3",
+    ],
+)
