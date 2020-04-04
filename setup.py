f68281faf707868e4f7d0992053f03f0c39298ac
Robert Huselius <robert@huseli.us>
2020-04-04 03:05:31 +0200

    Fix setup + gitignore + init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 
 
-with open(os.path.join(os.path.dirname(__file__)), "README.md", "r") as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as readme:
     long_description = readme.read()
 
 setuptools.setup(
@@ -14,14 +14,19 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Eboreg/twitterhal",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=["twitterhal"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 1 - Planning",
         "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Communications",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
     ],
     python_requires=">=3.6",
     install_requires=[
