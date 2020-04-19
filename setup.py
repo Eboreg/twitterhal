ae2341842cde3db44dddf4ee780994b03f08fef1
Robert Huselius <robert@huseli.us>
2020-04-19 23:04:58 +0200

    Update development status

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Communications",
